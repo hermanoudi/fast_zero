3d3355f17887be0d365b0e40c226c7a45d32ab99
Hermano Flávio de Moura <hermanoudi@gmail.com>
2025-01-13 18:56:06 -0300

    feat: :sparkles: Implementando o refresh do token e testes de autorização

--- tests/test_user.py
+++ tests/test_user.py
@@ -50,6 +50,20 @@ def test_update_user(client, user, token):
     }
 
 
+def test_update_user_with_wrong_user(client, other_user, token):
+    response = client.put(
+        f"/users/{other_user.id}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
+        },
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {"detail": "Not enough permissions"}
+
+
 def test_update_integrity_error(client, user, token):
     # Inserindo fausto
     client.post(
@@ -88,11 +102,10 @@ def test_delete_user(client, user, token):
     assert response.json() == {"message": "User deleted"}
 
 
-def test_delete_wrong_user(client, user, token):
+def test_delete_user_wrong_user(client, other_user, token):
     response = client.delete(
-        f"/users/{user.id + 1}",
+        f"/users/{other_user.id}",
         headers={"Authorization": f"Bearer {token}"},
     )
-
     assert response.status_code == HTTPStatus.FORBIDDEN
     assert response.json() == {"detail": "Not enough permissions"}
